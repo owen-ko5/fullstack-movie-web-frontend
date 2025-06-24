de7bb814eb59eda2a97da18e7fe353b81a70c9dd
Tyra <tyra.mwai@student.moringaschool.com>
2025-06-24 11:34:20 +0300

    correct import paths for components

--- src/components /CategoryButtons.jsx
+++ src/components /CategoryButtons.jsx
@@ -12,22 +12,4 @@ import Navbar from './components/MovieDetail';
 const App = ()=> {
     const token = localStorage.getitem('token');
 
-    return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/register" element={<Register />} />
-    <Route
-      path="/dashboard"
-      element={token ? <Dashboard /> : <Navigate to="/login" />}
-    />
-    <Route path="/home" element={<Home />} />
-    <Route path="/movies/:id" element={<MovieDetail />} />
-  </Routes>
-</Router>
-  );
-};
-
-export default App;
+    
